d29ad906947229d1c70d4129545b66b41f992ea2
kee <likj01@163.com>
2020-06-30 23:02:15 +0800

    nodejs 解决开发环境前后端跨域问题

--- vuecli4/src/views/Home.vue
+++ vuecli4/src/views/Home.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
-      WELCOME HOME !
+    <el-container>
+      <el-header class="homeHeader">
+        <div class="title">微人事</div>
+        <el-dropdown>
+          <span class="el-dropdown-link">
+            下拉菜单
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>黄金糕</el-dropdown-item>
+            <el-dropdown-item>狮子头</el-dropdown-item>
+            <el-dropdown-item>螺蛳粉</el-dropdown-item>
+            <el-dropdown-item disabled>双皮奶</el-dropdown-item>
+            <el-dropdown-item divided>蚵仔煎</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
+.homeHeader {
+  background-color: deepskyblue;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 15px;
+  box-sizing: border-box;
+}
 
+.homeHeader.title {
+  font-size: 30px;
+  font-family: "Franklin Gothic Medium";
+}
 </style>
